a380c9ea2a49983e02daffa953e39fa92aee3fbb
indywka <min4ik.edward@gmail.com>
2018-02-18 23:53:58 +0300

    +correct the removing the file in the local explorer --+adding reload TreeModel +adding upload in the popupmenu (-) when I use the menu cell INFO several times, then dialog boxes of all files appear    where the  method  INFO was called

--- src/FTP/FTPFile.java
+++ src/FTP/FTPFile.java
@@ -1,14 +1,32 @@
 package FTP;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
-public class FTPFile {
-    String absPath;
+public class FTPFile extends controllerFTP {
+    public String absPath;
     Date date;
     boolean exist = false;
+    protected String type;
+    private int returnCode;
 
-    FTPFile() {
+    public FTPFile(FTPFile parent, String child) {
+
+    }
+
+    public FTPFile() {
+    }
+
+    public String[] list() {
+        ArrayList<FTPFile> files = this.getFiles("/");
+        List<String> list = new ArrayList<String>();
+        for (FTPFile filek : files) {
+            list.add(filek.getName());
+        }
+        return list.toArray(new String[list.size()]);
     }
 
     public String getName() {
@@ -16,4 +34,21 @@ public class FTPFile {
         return indx == -1 ? this.absPath : this.absPath.substring(indx + 1, this.absPath.length());
     }
 
+    public boolean isDirectory() {
+        return this.type.equals("dir") || this.type.equals("cdir") || this.type.equals("pdir");
+    }
+
+    public boolean isFile() throws IOException {
+        return false;
+    }
+
+    public String getPath() {
+        int indx = this.absPath.lastIndexOf("/");
+        return indx == -1 || indx == 0 ? "/" : this.absPath.substring(0, indx);
+    }
+
+
+    public String getAbsPath() {
+        return this.absPath;
+    }
 }
