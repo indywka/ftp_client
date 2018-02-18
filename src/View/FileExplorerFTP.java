2eb3aeb521cf53f66715dd62ef12f4a2c2126383
indywka <min4ik.edward@gmail.com>
2018-02-18 23:40:53 +0300

    add tree for ftp server explorer

--- src/View/FileExplorerFTP.java
+++ src/View/FileExplorerFTP.java
@@ -3,28 +3,49 @@ package View;
 import FTP.FTPFile;
 import FTP.controllerFTP;
 
+import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileExplorerFTP extends FileExplorer {
-    private controllerFTP pi = null;
+
+    private DefaultMutableTreeNode root = new DefaultMutableTreeNode("CATALOGS_OF_THIS_SERVER");
 
     FileExplorerFTP() {
+        createTree();
+    }
+
+    @Override
+    protected void createTree() {
 
     }
 
     @Override
-    public void setPath(String path) {
+    public void setPath(String path) throws IOException {
         this.path = path;
-        this.model.clear();
-        if (this.pi == null) return;
+//        getDir(this.path);
+        buildTree("/", root);
+        tree = new JTree(root);
 
-        List<FTPFile> files = this.pi.getFiles(path);
-        for (FTPFile file : files) {
-            this.model.addElement(file.getName());
-        }
-        repaint();
-        updateUI();
+        this.add(tree);
     }
+
+    private List<String> getDir(String path) {
+        this.path = path;
+        tree.setModel(null);
+        ArrayList<FTPFile> files = this.pi.getFiles(path);
+        List<String> list = new ArrayList<>();
+        for (FTPFile file : files) {
+            list.add(file.getName());
+        }
+        System.out.println(list);
+        return list;
+    }
+
+    // так, я получил список директорий: List<String> list.
+    //теперь надо обойти дерево, т.е.
 
     void setPiFTP(controllerFTP pi) {
         this.pi = pi;
@@ -32,15 +53,33 @@ public class FileExplorerFTP extends FileExplorer {
     }
 
     @Override
-    protected void selected(String index) {
+    protected void selected(String index) throws IOException {
+        String path = this.path;
+        FTPFile file = this.pi.getFile(path);
+
+        if (file.isDirectory()) {
+            setPath(file.getAbsPath());
+        } else notifySelectedFile(path);
     }
 
     @Override
-    protected void delete(String path) {
+    protected void delete(String path) throws IOException {
+        if (!path.equals("..")) {
+            this.pi.delete(this.pi.getFile(path));
+            setPath(this.path);
+        }
+
     }
 
-    @Override
-    protected void createTree() {
+    private void buildTree(String currentdir, DefaultMutableTreeNode model) throws IOException {
 
+        List<String> currentCraw = getDir(currentdir);
+
+        for (String node : currentCraw) {
+            DefaultMutableTreeNode currentnode = new DefaultMutableTreeNode(node);
+//            buildTree(currentdir + "/" + node, currentnode);
+            model.add(currentnode);
+        }
     }
+
 }
